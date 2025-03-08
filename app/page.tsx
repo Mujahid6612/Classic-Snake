70861bdf5a1fe9b83ccbfc4ffb7ba6c01be64f06
Mujahid Ali <mujahidali6612@gmail.com>
2025-03-08 09:54:05 +0500

    Add mobile-specific description and app-ads.txt for Google AdMob

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Welcome to Classic Snake Game</h1>
+      <h6 className="text-lg text-center">
+        This is a simple snake game that you can play in your Android Phone
+      </h6>
     </div>
   );
 }
